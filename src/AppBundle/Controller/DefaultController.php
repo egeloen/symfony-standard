47358e6cec296757c6d00ef13fd1de3cbddce21e
Eric GELOEN <geloen.eric@gmail.com>
2017-03-06 21:23:37 +0100

    Give a try to egeloen/IvoryGoogleMapBundle#196

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\Type\DemoType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,6 +17,18 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/demo", name="demo")
+     */
+    public function demoAction(Request $request)
+    {
+        $form = $this->createForm(DemoType::class);
+
+        return $this->render('default/demo.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/AppBundle/Form/Type/DemoType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Ivory\GoogleMap\Event\Event;
+use Ivory\GoogleMap\Place\AutocompleteComponentType;
+use Ivory\GoogleMap\Place\AutocompleteType;
+use Ivory\GoogleMapBundle\Form\Type\PlaceAutocompleteType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class DemoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $event = new Event($variable = 'autocomplete', 'place_changed', <<<EOF
+function () {
+    console.log("place_changes triggered!");
+    
+    var place = autocomplete.getPlace();
+    console.log(place);
+}
+EOF
+);
+
+        $builder->add('location', PlaceAutocompleteType::class, [
+            'components' => [AutocompleteComponentType::COUNTRY => 'fr'],
+            'types'      => [AutocompleteType::GEOCODE],
+            'label'      => 'Where do you live.',
+            'attr'       => ['placeholder' => ''],
+            'variable'   => $variable,
+            'events'     => [$event],
+        ]);
+    }
+}
