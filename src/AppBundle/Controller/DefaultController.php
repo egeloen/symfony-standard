df44a53cb88a2a542c025fb6722b8a89d4b9f8a5
Eric GELOEN <geloen.eric@gmail.com>
2017-05-20 10:56:30 +0200

    Create a demo action + configure plugins

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use Ivory\CKEditorBundle\Form\Type\CKEditorType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -16,6 +18,21 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/demo", name="demo")
+     */
+    public function demoAction()
+    {
+        $form = $this->createFormBuilder()
+            ->add('description', CKEditorType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+
+        return $this->render('default/demo.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
