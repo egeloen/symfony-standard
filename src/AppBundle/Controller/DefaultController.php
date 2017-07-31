9f9ea483f92c3971625294b5b5ce60099a93b188
Eric GELOEN <geloen.eric@gmail.com>
2017-07-31 16:50:09 +0200

    Give a try to issue egeloen/IvoryCKEditorBundle#307

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use Ivory\CKEditorBundle\Form\Type\CKEditorType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends AbstractController
@@ -16,6 +18,23 @@ class DefaultController extends AbstractController
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/demo", name="demo")
+     */
+    public function demoAction(Request $request)
+    {
+        $form = $this->createFormBuilder()
+            ->add('text', CKEditorType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        return $this->render('default/demo.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
